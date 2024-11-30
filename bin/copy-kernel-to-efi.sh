3aa4b06fe5b81ea1e73e2a8355fcdb8c68f423eb
Jakub Mach-Gapski <j.mach.gapski@gmail.com>
2024-11-30 22:18:51 +0000

    Switched to using rEFind for Manjaro boot entry handling

--- bin/copy-kernel-to-efi.sh
+++ bin/copy-kernel-to-efi.sh
@@ -3,9 +3,9 @@
 VMLINUZ=vmlinuz-*
 INITRAMFS=initramfs-*{x86_64,lqx}.img
 
-eval rm -v /boot/efi/EFI/ManjaroKernel/$VMLINUZ /boot/efi/EFI/ManjaroKernel/$INITRAMFS
+eval rm -v /boot/efi/EFI/Manjaro/$VMLINUZ /boot/efi/EFI/Manjaro/$INITRAMFS
 for f in /boot/$VMLINUZ; do
-    cp -v -- "$f" "/boot/efi/EFI/ManjaroKernel/$(basename $f).efi"
+    cp -v -- "$f" "/boot/efi/EFI/Manjaro/$(basename $f).efi"
 done
-eval cp -v /boot/$INITRAMFS /boot/efi/EFI/ManjaroKernel/
-cp -v /boot/amd-ucode.img /boot/efi/EFI/ManjaroKernel/
+eval cp -v /boot/$INITRAMFS /boot/efi/EFI/Manjaro/
+cp -v /boot/amd-ucode.img /boot/efi/EFI/Manjaro/

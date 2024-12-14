e4486ff08f19f0c4ce3b4ee564fc5a5d8f13cbb0
Jakub Mach-Gapski <j.mach.gapski@gmail.com>
2024-12-14 20:15:14 +0000

    copy script now no longer copies fallback initramfs as well

--- bin/copy-kernel-to-efi.sh
+++ bin/copy-kernel-to-efi.sh
@@ -2,12 +2,14 @@
 
 VMLINUZ=vmlinuz-*
 INITRAMFS=initramfs-*{x86_64,lqx}.img
-INITRAMFS_FALLBACK=initramfs-*{x86_64,lqx}-fallback.img
+# Uncomment after all installs using this have EFI partition that fits at least 2 kernels (4 images) worth of initramfs
+# INITRAMFS_FALLBACK=initramfs-*{x86_64,lqx}-fallback.img
 
 eval rm -v /boot/efi/EFI/Manjaro/$VMLINUZ /boot/efi/EFI/Manjaro/$INITRAMFS /boot/efi/EFI/Manjaro/$INITRAMFS_FALLBACK
+# eval rm -v /boot/efi/EFI/Manjaro/$VMLINUZ /boot/efi/EFI/Manjaro/$INITRAMFS /boot/efi/EFI/Manjaro/$INITRAMFS_FALLBACK
 for f in /boot/$VMLINUZ; do
     cp -v -- "$f" "/boot/efi/EFI/Manjaro/$(basename $f).efi"
 done
 eval cp -v /boot/$INITRAMFS /boot/efi/EFI/Manjaro/
-eval cp -v /boot/$INITRAMFS_FALLBACK /boot/efi/EFI/Manjaro/
+# eval cp -v /boot/$INITRAMFS_FALLBACK /boot/efi/EFI/Manjaro/
 cp -v /boot/amd-ucode.img /boot/efi/EFI/Manjaro/
